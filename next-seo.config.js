35bce9e04da28fa28d741d78e0758004f8238fad
viswagfx <91825731+viswagfx@users.noreply.github.com>
2024-05-10 19:56:21 +0530

    Update next-seo.config.js

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,8 +1,8 @@
 export default {
     openGraph: {
-        type: 'VisTesting',
+        type: 'Vis',
         locale: 'en_IE',
-        title: 'Testing For WOOO',
+        title: 'viswagfx',
         url: 'https://previews.123rf.com/images/imagecatalogue/imagecatalogue1611/imagecatalogue161116759/66637610-test-text-rubber-seal-stamp-watermark-tag-inside-rounded-rectangular-banner-with-grunge-design-and.jpg',
         description: 'WOOO',
         locale: 'en_EN',
